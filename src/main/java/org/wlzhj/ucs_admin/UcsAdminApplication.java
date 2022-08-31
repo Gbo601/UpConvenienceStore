f0b489753563271976889bb7e720d4594085cb35
Gbo601 <1520389125@qq.com>
2022-08-31 15:40:53 +0800

    init

---
+++ src/main/java/org/wlzhj/ucs_admin/UcsAdminApplication.java
@@ -0,0 +1,13 @@
+package org.wlzhj.ucs_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UcsAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UcsAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/wlzhj/ucs_admin/UcsAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wlzhj.ucs_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UcsAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
